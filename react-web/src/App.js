5cb168b9800fe7bd945d1bbce3b1902f7bdfbbdc
Ilakkiyan <ilakkiyanmohanraj@gmail.com>
2024-09-17 21:21:45 +0530

    feauter signup working

---
+++ react-web/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Services from './components/Services';
+import Products from './components/Product';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
